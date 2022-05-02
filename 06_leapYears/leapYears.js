20f0435e4944edc3eed2cf627b602cc96319f1a5
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-02 15:37:32 +0530

    Complete leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  return new Date(`February 29, ${year}`).getDate() === 29 ? true : false;
 };
 
 // Do not edit below this line
