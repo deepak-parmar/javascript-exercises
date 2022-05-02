72b869b25fb122a11015e4b40f75af750a91d977
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-02 15:34:29 +0530

    Complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (startingNum, endingNum) {
+  if (
+    typeof startingNum != "number" ||
+    typeof endingNum != "number" ||
+    startingNum < 0 ||
+    endingNum < 0
+  )
+    return "ERROR";
 
+  // swap values of endingNum is smaller than startinNum
+  if (endingNum < startingNum)
+    [startingNum, endingNum] = [endingNum, startingNum];
+
+  let sum = 0;
+  for (let n = startingNum; n <= endingNum; n++)
+    sum += n;
+
+  return sum;
 };
 
 // Do not edit below this line
