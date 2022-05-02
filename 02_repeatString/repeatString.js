bcd012c34fe5d2c4e496d145460751dd48542b83
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-02 15:01:26 +0530

    Complete repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (inputString, times) {
+  if (times < 0) return "ERROR";
 
+  let outputString = '';
+  for (let i = 0; i < times; i++) {
+    outputString += inputString;
+  }
+
+  return outputString;
 };
 
 // Do not edit below this line
