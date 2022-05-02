75b1577c3c91aa504a9641f91f03ca81304ee413
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-02 15:12:14 +0530

    Complete reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (inputString) {
+  let reversedString = '';
 
+  for (let i = inputString.length - 1; i >= 0; i--)
+    reversedString += inputString[i];
+
+  return reversedString;
 };
+
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
