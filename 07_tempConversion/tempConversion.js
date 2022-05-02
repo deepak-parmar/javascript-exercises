7c69199141430c3996c5705ca9cf60246c8bcbb6
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-02 16:10:17 +0530

    Complete ctof and ftoc functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,25 @@
-const ftoc = function() {
+const ftoc = function (fTemp) {
+  cTemp = (fTemp - 32) * (5 / 9);
 
+  if (Number.isInteger(cTemp))
+    return cTemp;
+
+  return parseFloat(cTemp.toFixed(1))
 };
 
-const ctof = function() {
+const ctof = function (cTemp) {
+  fTemp = cTemp * (9 / 5) + 32;
 
+  if (Number.isInteger(fTemp))
+    return fTemp;
+
+  return parseFloat(fTemp.toFixed(1))
 };
+
+console.log(ctof(0));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
