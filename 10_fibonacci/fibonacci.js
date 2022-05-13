904453a199f23895a6593807716b4c81f4b525c5
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-13 14:06:32 +0530

    Finish fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num)
+  
+  if (num < 0) return "OOPS"
+
+  if (!isNaN(num))
+    if (num <= 1) return num
+    else return fibonacci(num-1) + fibonacci(num-2)
+  else return "OOPS"
 
 };
 
