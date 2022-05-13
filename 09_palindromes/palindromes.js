1f18977d151c277232dc383539ab36f6e0e9c638
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-13 18:05:46 +0530

    Finish palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+  let wordRegex = /[^\W\_\,\.\(\:\)\-\|\\\/ ]/gi
+  let wordClean = words.match(wordRegex).join('').toLowerCase()
+  let wordReverse = wordClean.split('').reverse().join('')
+  return wordClean === wordReverse
 };
+
+palindromes("A car, a! man, a maraca.")
 
 // Do not edit below this line
 module.exports = palindromes;
