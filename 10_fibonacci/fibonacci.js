f4169a3fecf3ab8cf7d19dac919440df04af0cf0
Deepak Parmar <dgparmar1999@rediffmail.com>
2023-07-26 10:15:51 +0530

    Refactor 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,9 @@
 const fibonacci = function (num) {
-  num = Number(num)
-  
-  if (num < 0) return "OOPS"
+  num = Number(num);
 
-  if (!isNaN(num))
-    if (num <= 1) return num
-    else return fibonacci(num-1) + fibonacci(num-2)
-  else return "OOPS"
+  if (num < 0 || isNaN(num)) return "OOPS";
 
+  return num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
