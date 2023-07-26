8c70133e3a115b15e3f53d32046ad8b1d97d926d
Deepak Parmar <dgparmar1999@rediffmail.com>
2023-07-26 12:27:18 +0530

    Solve 12_findTheOldest and unskip all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // Calculate and add age to every person
+  people = people.map((person) => {
+    person.age = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : new Date().getFullYear() - person.yearOfBirth;
+    return person;
+  });
+  const oldestAge = Math.max(...people.map((p) => p.age));
+  return people.find((p) => p.age === oldestAge);
 };
 
 // Do not edit below this line
