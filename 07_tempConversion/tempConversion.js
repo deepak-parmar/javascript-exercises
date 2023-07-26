ec932a60291c8dc020266ac7bafb56ea2a1c42b0
Deepak Parmar <dgparmar1999@rediffmail.com>
2023-07-26 10:08:03 +0530

    Refactor 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,22 +1,12 @@
 const ftoc = function (fTemp) {
-  cTemp = (fTemp - 32) * (5 / 9);
-
-  if (Number.isInteger(cTemp))
-    return cTemp;
-
-  return parseFloat(cTemp.toFixed(1))
+  const cTemp = (fTemp - 32) * (5 / 9);
+  return Number.isInteger(cTemp) ? cTemp : parseFloat(cTemp.toFixed(1));
 };
 
 const ctof = function (cTemp) {
-  fTemp = cTemp * (9 / 5) + 32;
-
-  if (Number.isInteger(fTemp))
-    return fTemp;
-
-  return parseFloat(fTemp.toFixed(1))
+  const fTemp = cTemp * (9 / 5) + 32;
+  return Number.isInteger(fTemp) ? fTemp : parseFloat(fTemp.toFixed(1));
 };
-
-console.log(ctof(0));
 
 // Do not edit below this line
 module.exports = {
